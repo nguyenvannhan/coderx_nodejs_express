49e0f92680d3df6c5c5f6b12a350e4b2537d4777
Van Nhan <nguyenvannhan0810@gmail.com>
2020-03-03 16:53:22 +0700

    Finish Lession 3: Query Parameter

--- 1-hello/index.js
+++ 1-hello/index.js
@@ -5,16 +5,32 @@ const port = 3000;
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+var users = [
+    { id: 1, name: 'Thinh' },
+    { id: 2, name: 'Hung' },
+];
+
 app.get('/', (req, res) => {
     res.render('index', { title: 'Hello World' });
 });
 
 app.get('/users', (req, res) => {
     res.render('users/index', {
-        users: [
-            { id: 1, name: 'Thinh' },
-            { id: 2, name: 'Hung' },
-        ],
+        users: users,
+        queryString: '',
+    });
+});
+
+app.get('/users/search', (req, res) => {
+    var q = req.query.q;
+
+    var matchedUsers = users.filter(user => {
+        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+    });
+
+    res.render('users/index', {
+        users: matchedUsers,
+        queryString: q,
     });
 });
 
