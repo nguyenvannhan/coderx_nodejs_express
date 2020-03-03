98cd60a18eff0b01e21ae0b4b3b48d11a44b7fd7
Van Nhan <nguyenvannhan0810@gmail.com>
2020-03-03 16:28:43 +0700

    Finish Lesson 2: Use Pug Template Engine

--- 1-hello/index.js
+++ 1-hello/index.js
@@ -2,6 +2,20 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello world'));
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Hello World' });
+});
+
+app.get('/users', (req, res) => {
+    res.render('users/index', {
+        users: [
+            { id: 1, name: 'Thinh' },
+            { id: 2, name: 'Hung' },
+        ],
+    });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
