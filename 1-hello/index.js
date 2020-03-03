4c416effb4b8c7c14b134c01398e526bf8ee16be
Van Nhan <nguyenvannhan0810@gmail.com>
2020-03-03 21:07:39 +0700

    Finish Lesson 4: Post Method

--- 1-hello/index.js
+++ 1-hello/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
 app.set('view engine', 'pug');
 app.set('views', './views');
+
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 var users = [
     { id: 1, name: 'Thinh' },
@@ -32,6 +36,16 @@ app.get('/users/search', (req, res) => {
         users: matchedUsers,
         queryString: q,
     });
+});
+
+app.get('/users/create', (req, res) => {
+    res.render('users/create');
+});
+
+app.post('/users/create', (req, res) => {
+    users.push(req.body);
+
+    res.redirect("/users");
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
