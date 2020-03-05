76649cc0fdcf4ce71fe3c8a8c2e2d742574a7c18
Van Nhan <nguyenvannhan0810@gmail.com>
2020-03-05 13:49:20 +0700

    Finish lesson 13: Middleware

--- 1-hello/routes/user.route.js
+++ 1-hello/routes/user.route.js
@@ -2,6 +2,7 @@ var express = require('express');
 
 var router = express.Router();
 var controller = require('../controllers/user.controller');
+var validate = require('../validate/user.validate');
 
 router.get('/', controller.index);
 
@@ -9,7 +10,7 @@ router.get('/search', controller.search);
 
 router.get('/create', controller.create);
 
-router.post('/create', controller.postCreate);
+router.post('/create', validate.postCreate, controller.postCreate);
 
 router.get('/:id', controller.get);
 
